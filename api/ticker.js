12a59d56a23535470db78fc7717e468078113d81
evrentanyeri <evrentanyeri@gmail.com>
2025-11-15 17:16:03 +0300

    Update ticker.js

--- api/ticker.js
+++ api/ticker.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
     try {
-        const r = await fetch("https://contract.mexc.com/api/v1/contract/detail");
+        const r = await fetch("https://contract.mexc.com/api/v1/contract/details");
         const json = await r.json();
         res.status(200).json(json);
     } catch (err) {
