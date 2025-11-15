b350ffdc3e7aca5af9eedb6506c3cf0032888872
evrentanyeri <evrentanyeri@gmail.com>
2025-11-15 16:52:19 +0300

    Create ticker.js

---
+++ api/ticker.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+    try {
+        const r = await fetch("https://contract.mexc.com/api/v1/contract/ticker");
+        const json = await r.json();
+        res.status(200).json(json);
+    } catch (err) {
+        res.status(500).json({ error: "MEXC bağlantı hatası", details: err.toString() });
+    }
+}
