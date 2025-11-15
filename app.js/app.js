9375d7eac878fe25ddd1ab299ba2e5714c46d66d
evrentanyeri <evrentanyeri@gmail.com>
2025-11-15 15:07:15 +0300

    Add files via upload

---
+++ app.js/app.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const tbody = document.getElementById("cryptoBody");
+
+    // Şimdilik placeholder aktif
+    // API bağlantısını sen "hazırım" dediğinde ekleyeceğim
+});
