1d2ad8f024fc9c75c8ec67c51c0a65753207d6a0
evrentanyeri <evrentanyeri@gmail.com>
2025-11-15 15:36:20 +0300

    Delete app.js directory

--- app.js/app.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const tbody = document.getElementById("cryptoBody");
-
-    // Şimdilik placeholder aktif
-    // API bağlantısını sen "hazırım" dediğinde ekleyeceğim
-});
